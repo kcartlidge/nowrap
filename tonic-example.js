dc9d8cc177f0263104560f5d715a8c0c7e5d1888
Karl Cartlidge <code@kcartlidge.com>
2015-12-09 22:39:28 +0000

    Updated tonic-example for clarity when run online.

--- tonic-example.js
+++ tonic-example.js
@@ -1,16 +1,23 @@
+/*
+ Tonic dumps the final result to the console itself, which
+ is why it shows again following the 'after' hook.
+ */
+
 var wrap = require('nowrap');
 
 // Load a module to use as an example.
 var path = require('path');
 
 // Create a function to be called before a wrapped one.
-var beforeFunc = function(name, args) {
-	console.log("Calling '" + name + "' with ", args);
+var beforeFunc = function (name, args) {
+	console.log("Calling '" + name + ".");
+	console.log(args);
 };
 
 // Create a function to be called after a wrapped one.
-var afterFunc = function(name, result) {
-	console.log("It returned the caller ", result);
+var afterFunc = function (name, result) {
+	console.log("It returned from the caller.");
+	console.log(result);
 };
 
 // Wrap the main functions and re-run.
